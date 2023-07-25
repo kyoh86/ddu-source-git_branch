ab87303d9d57e02b8b926cdb68ae9de3304ac5ae
kyoh86 <me@kyoh86.dev>
2023-07-26 00:21:16 +0900

    close stream controller

--- denops/@ddu-sources/git_branch.ts
+++ denops/@ddu-sources/git_branch.ts
@@ -127,7 +127,9 @@ export class Source extends BaseSource<Params, ActionData> {
                 );
               },
             }),
-          );
+          ).finally(() => {
+            controller.close();
+          });
       },
     });
   }
