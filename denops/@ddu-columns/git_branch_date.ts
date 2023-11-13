7f85e2d057d685f35360d8534dec1a23ef86bd29
kyoh86 <me@kyoh86.dev>
2023-11-14 02:06:32 +0900

    update dependencies

--- denops/@ddu-columns/git_branch_date.ts
+++ denops/@ddu-columns/git_branch_date.ts
@@ -3,8 +3,8 @@ import type {
 } from "https://deno.land/x/ddu_vim@v3.6.0/types.ts";
 import { ActionData } from "../@ddu-kinds/git_branch.ts";
 import { GitBranchBaseColumn } from "./git_branch_base.ts";
-import type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
-import { strwidth } from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
+import type { Denops } from "https://deno.land/x/denops_std@v5.0.2/mod.ts";
+import { strwidth } from "https://deno.land/x/denops_std@v5.0.2/function/mod.ts";
 
 export class Column extends GitBranchBaseColumn {
   override async getAttr(denops: Denops, { date }: ActionData): Promise<{
