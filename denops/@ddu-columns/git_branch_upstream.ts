4698434cc091a0dde59bdc28d4665cb72439bd03
kyoh86 <me@kyoh86.dev>
2024-01-21 20:00:24 +0900

    Update dependencies

--- denops/@ddu-columns/git_branch_upstream.ts
+++ denops/@ddu-columns/git_branch_upstream.ts
@@ -1,10 +1,10 @@
 import type {
   ItemHighlight,
-} from "https://deno.land/x/ddu_vim@v3.9.0/types.ts";
+} from "https://deno.land/x/ddu_vim@v3.10.0/types.ts";
 import { ActionData } from "../@ddu-kinds/git_branch.ts";
 import { GitBranchBaseColumn } from "./git_branch_base.ts";
-import type { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
-import { strwidth } from "https://deno.land/x/denops_std@v5.2.0/function/mod.ts";
+import type { Denops } from "https://deno.land/x/denops_std@v5.3.0/mod.ts";
+import { strwidth } from "https://deno.land/x/denops_std@v5.3.0/function/mod.ts";
 
 export class Column extends GitBranchBaseColumn {
   override async getAttr(denops: Denops, { upstream }: ActionData): Promise<{
