0db245c113d2059678329b49648c9b9aaa1bf973
GitHub <noreply@github.com>
2024-10-26 09:01:17 +0000

    chore(deps): Update deno dependencies

--- denops/@ddu-columns/git_branch_base.ts
+++ denops/@ddu-columns/git_branch_base.ts
@@ -1,11 +1,11 @@
-import type { DduItem, ItemHighlight } from "jsr:@shougo/ddu-vim@~6.1.0/types";
-import { BaseColumn } from "jsr:@shougo/ddu-vim@~6.1.0/column";
+import type { DduItem, ItemHighlight } from "jsr:@shougo/ddu-vim@~6.2.0/types";
+import { BaseColumn } from "jsr:@shougo/ddu-vim@~6.2.0/column";
 import type {
   GetTextArguments,
   GetTextResult,
-} from "jsr:@shougo/ddu-vim@~6.1.0/column";
-import type { Denops } from "jsr:@denops/std@~7.1.0";
-import * as fn from "jsr:@denops/std@~7.1.0/function";
+} from "jsr:@shougo/ddu-vim@~6.2.0/column";
+import type { Denops } from "jsr:@denops/std@~7.3.0";
+import * as fn from "jsr:@denops/std@~7.3.0/function";
 import type { ActionData } from "../@ddu-kinds/git_branch.ts";
 
 type Params = {
