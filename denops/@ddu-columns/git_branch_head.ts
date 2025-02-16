bf9a0e4567f1671a01f4a34ec82dde84719f3808
kyoh86 <me@kyoh86.dev>
2025-02-16 22:40:51 +0900

    Update dependencies

--- denops/@ddu-columns/git_branch_head.ts
+++ denops/@ddu-columns/git_branch_head.ts
@@ -3,7 +3,7 @@ import {
   BaseColumn,
   type GetTextArguments,
   type GetTextResult,
-} from "jsr:@shougo/ddu-vim@~9.5.0/column";
+} from "jsr:@shougo/ddu-vim@~10.0.0/column";
 
 type Params = Record<PropertyKey, never>;
 
